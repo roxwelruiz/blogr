e609fd6c99f6b4299da15ba2b2699f562c25763c
roxwelruiz <roxwel.ruiz@gmail.com>
2017-03-02 19:22:18 -0500

    4.6.2 Add About and Contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'main_page#index'
+  get '/about', to: 'main_page#about'
+	get '/contact', to: 'main_page#contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
